a07bb59aea82b15a7f2393d38614d595f9a3ca3f
[hyunba] <dls272@naver.com>
2022-02-13 03:55:45 +0900

    todo

---
+++ react_todo/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import { darkTheme } from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+        <ThemeProvider theme={darkTheme}>
+          <App />
+        </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
