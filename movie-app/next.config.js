e14c5766aec5b0f4a6ae8e960a0a0950e54aa90e
[hyunba] <dls272@naver.com>
2022-02-20 23:45:10 +0900

    Practice NextJS Project

--- movie-app/next.config.js
+++ movie-app/next.config.js
@@ -1,3 +1,20 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
+  async redirect() {
+    return [
+      {
+        source: "/contact*", // 사용자가 이쪽으로오면 *을 사용하면 뒤 내용도 다 똑같음
+        destination: "/form*", // 이쪽으로 보내진다.
+        permanent: false,
+      }
+    ]
+  },
+  async rewrites() {
+    return [{
+      source: "/api/movies", // redirect는 사용자가 source 주소와 destination 주소를 알게된다. 하지만 rewrites는 사용자에게 source 주소만 보여준다.
+      destination:`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+    }]
+  }
 }
--- movie-app/pages/index.js
+++ movie-app/pages/index.js
@@ -1,24 +1,44 @@
 import { useEffect, useState } from "react";
 
-const API_KEY = "659fccb8edf938abc045369e7c13761a";
-
 export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+      const response = await fetch(`/api/movies`);
       const { results } = await response.json();
       // 같은 의미 const response = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();/
       setMovies(results);
     })();
   })
   return (
-    <div>
+    <div className="container">
       {!movies && <h4>Loading...</h4>}
       {movies.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>))}
+        <style jsx>{`
+          .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: 20px;
+            gap: 20px;
+          }
+          .movie img {
+            max-width: 100%;
+            border-radius: 12px;
+            transition: transform 0.2s ease-in-out;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          }
+          .movie:hover img {
+            transform: scale(1.05) translateY(-10px);
+          }
+          .movie h4 {
+            font-size: 18px;
+            text-align: center;
+          }
+      `}</style>
     </div>
   );
 }
