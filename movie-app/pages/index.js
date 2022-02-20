6b8968d41f1fb5cff67e7c1741b083df7ab73f2a
[hyunba] <dls272@naver.com>
2022-02-21 01:16:12 +0900

    Practice NextJS Project

--- movie-app/pages/index.js
+++ movie-app/pages/index.js
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
-
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`/api/movies`);
-      const { results } = await response.json();
-      // 같은 의미 const response = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();/
-      setMovies(results);
-    })();
-  })
+export default function Home({results}) {
   return (
     <div className="container">
-      {!movies && <h4>Loading...</h4>}
-      {movies.map((movie) => (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -41,4 +30,12 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(`http://localhost:3000/api/movies`);
+  const { results } = await response.json();
+  return {
+    props : {results},
+  };
 }
