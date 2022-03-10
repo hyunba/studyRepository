63674053376ace04925f3bf6a21d1ceef8f903da
[hyunba] <dls272@naver.com>
2022-03-11 00:20:02 +0900

    Framer-motion SVG Animation Practice

--- Framer_motion/src/App.tsx
+++ Framer_motion/src/App.tsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 const Wrapper = styled(motion.div)`
   display: flex;
   width: 100vw;
-  height: 200vh;
+  height: 100vh;
   justify-content: center;
   align-items: center;
   background: gray;
@@ -130,8 +130,8 @@ const circleVariants = {
 };
 
 const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
   path {
     stroke: white;
     stroke-width: 2;
@@ -182,6 +182,23 @@ function App() {
       </BiggerBox>
 
       <Box3 style={{x, scale: transValue}} drag variants={box3Variants} whileHover="hover" whileTap="click" whileDrag="drag" />
+
+      <Svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+          <motion.path
+            variants={svg}
+            initial={"start"}
+            animate={"end"}
+            transition={{ default: {duration: 5}, fill: {duration: 2, delay: 5}, }}
+            // default는 모든 속성 적용 fill만 따로 적용하기 위해 사용
+            fill="transparent" 
+            d="M86.642 0L122 94.469H0l3.914-11.741L35.494 0h14.44l10.391 28.88L72.201 0h14.44zM49.934 83.942h20.783L66.803 74.9l-6.478-17.139-10.391 26.181zM43.32 9.312l-28.88 74.63h24.966l1.215-1.214 14.44-39.407-3.914-10.527-7.827-23.482zm62.89 74.63l-2.7-9.042-7.827-19.838-7.828-22.268L78.68 9.312 65.59 43.32l15.654 40.621h24.967z"
+          >
+          </motion.path>
+      </Svg>
 
     </Wrapper>
   );
