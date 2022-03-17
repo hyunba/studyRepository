8644c95a6db9a4c101666382413185c999b5c82a
[hyunba] <dls272@naver.com>
2022-03-17 23:23:08 +0900

    Framer-motion layout UI Practice

--- Framer_motion/src/App4.tsx
+++ Framer_motion/src/App4.tsx
@@ -17,6 +17,13 @@ const Circle = styled(motion.div)`
   width: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+const Circle2 = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 const Box = styled(motion.div)`
   width: 300px;
@@ -29,6 +36,17 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Box2 = styled(motion.div)`
+  width: 300px;
+  height: 300px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 
 // framer motion을 div나 span에 하기 위해서는 motion.div, motion.span으로 지정해주어야한다.
 function App() {
@@ -37,7 +55,7 @@ function App() {
   return (
     <Wrapper>
       <Box onClick={togglebtn}>
-        {!clicked ? (
+        {!clicked ? ( //둘의 UI는 같다는 것을 의미하기 위해 layoutId를 이름을 동일하게 함.
           <Circle layoutId="circle" style={{ borderRadius: 50 }} />
         ) : null}
       </Box>
@@ -46,6 +64,10 @@ function App() {
           <Circle layoutId="circle" style={{ borderRadius: 0, scale: 1 }} />
         ) : null}
       </Box>
+
+      <Box2 onClick={togglebtn} style={{ justifyContent: clicked ? "center" : "flex-start", alignItems: clicked ? "center":"flex-start", }}>
+        <Circle2 layout />
+      </Box2>
     </Wrapper>
   );
 }
