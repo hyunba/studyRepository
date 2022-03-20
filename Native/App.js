59b7bd9d49cf518f58c52677fca7a438b602eb04
[hyunba] <dls272@naver.com>
2022-03-20 21:09:01 +0900

    React Native Emulator Practice

--- Native/App.js
+++ Native/App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { theme } from './color';
+
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const[text, setText] = useState("");
+  const example1 = () => setWorking(false);
+  const example2 = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
-      <Text>Hi! Native App</Text>
-      <Text>Next Text!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={example1}>
+          <Text style={{...styles.btnText, color: working ? "blue" : theme.gray }}>I'm False</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={example2}>
+          <Text style={{...styles.btnText, color: !working ? "blue" : theme.gray }}>I'm True</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput 
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working ? "Falese" : "True"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -17,5 +40,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "gray",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
---
+++ Native/color.js
@@ -0,0 +1,4 @@
+export const theme = {
+    bg: "black",
+    gray: "#3A3D40"
+}
