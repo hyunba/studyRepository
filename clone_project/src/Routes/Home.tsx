896d26b450861aacebb356f8b1219945d6d59472
[hyunba] <dls272@naver.com>
2022-04-11 23:22:08 +0900

    Clone Webpage

--- clone_project/src/Routes/Home.tsx
+++ clone_project/src/Routes/Home.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { useQuery } from "react-query";
 import { getMovies, GetMoviesResult } from "../api";
 import { makeImagePath } from "./utils";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled.div`
     background: black;
@@ -55,10 +56,22 @@ const Box = styled(motion.div)`
     background-color: white;
     height: 200px;
 `;
-// 4:46
+const rowVariants = {
+    hidden: {
+        x: 1000,
+    },
+    visible: {
+        x: 0
+    },
+    exit: {
+        x: -1000,
+    },
+}
+//08:05
 function Home() {
     const { data, isLoading } = useQuery<GetMoviesResult>(["movies", "nowPlaying"], getMovies);
-    console.log(data, isLoading);
+    const [index, setIndex] = useState(0);
+    const increaseIndex = () => setIndex((prev) => prev+1);
     return (
         <Wrapper>
             {isLoading ? (
@@ -67,19 +80,21 @@ function Home() {
                 </Loader>
                 ) : (
                     <>
-                        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                        <Banner onClick={increaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
                             <Title>{data?.results[0].title}</Title>
                             <Overview>{data?.results[0].overview}</Overview>
                         </Banner>
                         <Slider>
-                            <Row>
-                                <Box />
-                                <Box />
-                                <Box />
-                                <Box />
-                                <Box />
-                                <Box />
-                            </Row>
+                            <AnimatePresence>
+                                <Row variants={rowVariants} initial="hidden" animate="visible" exit="exit" key={index}>
+                                    <Box />
+                                    <Box />
+                                    <Box />
+                                    <Box />
+                                    <Box />
+                                    <Box />
+                                </Row>
+                            </AnimatePresence>
                         </Slider>
                     </>
                 )}
