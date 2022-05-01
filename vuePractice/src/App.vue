4f9f5320029274b22108fa49d347d56d9db457c2
[hyunba] <dls272@naver.com>
2022-05-01 23:26:09 +0900

    Vue Practice

--- vuePractice/src/App.vue
+++ vuePractice/src/App.vue
@@ -1,16 +1,20 @@
+// template 안에는 HTML로 이루어져있다.
 <template>
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    우와 신기해요!
   </div>
 </template>
 
+// script 안에는 JS로 이루어져있다.
 <script>
 export default {
   name: 'App'
 }
 </script>
 
+// style 안에는 CSS로 이루어져있다.
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
