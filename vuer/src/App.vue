b18a41abda48034251cd692c892cc3475ae3ad6d
[hyunba] <dls272@naver.com>
2022-05-28 23:34:29 +0900

    Vue Practice

--- vuer/src/App.vue
+++ vuer/src/App.vue
@@ -1,31 +1,60 @@
 <template>
-  <div>
-    <MyPage/>
-
-
+  <div class="modal-bg" v-if="modal === true">
+    <div class="modal-inner">
+      <h4> This is Modal Page </h4>
+      <p> Modal Page Content </p>
+      <button @click="modalOnOff">✔️</button>
+    </div>
   </div>
-
+  <div v-for="(d, i) in products" :key="i">
+    <h4>{{d}}</h4>
+    <p>Vue Practice</p>    
+  </div>
+  <div>
+    <button @click="increase">❤️</button> <span>좋아요 : {{like}}</span>
+    <br>
+    <br>
+    <button @click="modalOnOff">✔️</button> <span>모달창 : {{modal}}</span>
+  </div>
 </template>
 
 <script>
-import MyPage from "./components/MyPage.vue";
-
 
 export default {
   name: 'App',
   data(){
     return {
-      
-
+      modal: false,
+      like: 0,
+      products : ['hi', 'hihi', 'hihihi']  
     }
   },
-  components: {
-    
-    MyPage
-}
+  methods : {
+    increase() {
+      this.like += 1;
+    },
+    modalOnOff() {
+      this.modal = !this.modal
+    },
+  },
 }
 </script>
 
 <style>
-
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.modal-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.modal-inner {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 </style>
