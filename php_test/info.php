87a0405dd37b4f234d4ee612036d569818430f64
[hyunba] <dls272@naver.com>
2022-06-18 23:42:03 +0900

    php practice를 위한 환경설정 및 세팅

---
+++ php_test/info.php
@@ -0,0 +1,13 @@
+<?php
+phpinfo();
+?>
+<!--
+    Start Menu\Programs\WAMP packaged by Bitnami
+    bitnami manage tool 
+
+    실제 php가 화면으로 부여주는 경로 \Bitnami\wampstack-8.1.7-0\apache2\htdocs
+
+    아파치가 설치되어있어서 localhost로 웹사이트를 들어갈 수 있다.
+
+    debug extention 하려면 setting에서 php.exe 경로 넣어주어야함
+-->
