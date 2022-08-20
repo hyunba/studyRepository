8d9d17b9649a978e4d87eaea096d903e669a1c65
[hyunba] <dls272@naver.com>
2022-08-20 23:38:00 +0900

    svelte tutorial

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	let name = 'Lee';
+	let src = 'https://svelte.dev/tutorial/image.gif';
+	let string = `this string contains some <strong>hihi</strong>`;// {@html}은 html 그대로 출력해줌
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img {src} alt="A img">
+	<p>haha {@html string}</p>
 </main>
 
 <style>
@@ -13,6 +17,11 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
 	}
 
 	h1 {
